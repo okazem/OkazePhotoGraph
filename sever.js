0a8fd4f830ce1a4c0da6d1f276ea518675439a6d
DESKTOP-1OTU587\Owner <yudai.forest929@gmail.com>
2023-03-07 23:29:17 +0900

    queryでフォルダIDを設定できるようにした。

--- sever.js
+++ sever.js
@@ -1,34 +1,38 @@
-const express = require('express');
-const request = require('request');
-const fetch = require('node-fetch');
+const express = require("express");
+const request = require("request");
+const fetch = require("node-fetch");
 const app = express();
 
 // Google Drive URL of the image you want to retrieve
 const GAS_URL = process.env.GAS_URL;
 
 // Set response headers
-app.get('/image.png', (req, res) => {
-    console.log("req");
-    let FILE_URL = "";
-    try {
-      fetch(GAS_URL)
+app.get("/image.png", (req, res) => {
+  console.log("req");
+  let FILE_URL = "";
+  try {
+    fetch(GAS_URL+"?folder="+req.query.folderId)
       .then((response) => response.json())
       .then((data) => {
-          res.setHeader('Content-Type', 'image/png');
-          res.setHeader('Content-Disposition', `inline; filename="image.png"`);
-          // Pipe the image data to the response
-          request.get(`http://drive.google.com/uc?export=view&id=${data.id}`).pipe(res);
-      })}
-    catch(error) {
-      res.setHeader('Content-Type', 'image/png');
-      res.setHeader('Content-Disposition', `inline; filename="image.png"`);
-      console.error(error);
-      // Pipe the image data to the response
-      request.get(`http://drive.google.com/uc?export=view&id=1Ec3FESUZT0QlMRKQ-CyR-orw65j63sp1`).pipe(res);
-      }
-    }
-
-);
+        res.setHeader("Content-Type", "image/png");
+        res.setHeader("Content-Disposition", `inline; filename="image.png"`);
+        // Pipe the image data to the response
+        request
+          .get(`http://drive.google.com/uc?export=view&id=${data.id}`)
+          .pipe(res);
+      });
+  } catch (error) {
+    res.setHeader("Content-Type", "image/png");
+    res.setHeader("Content-Disposition", `inline; filename="image.png"`);
+    console.error(error);
+    // Pipe the image data to the response
+    request
+      .get(
+        `http://drive.google.com/uc?export=view&id=1Ec3FESUZT0QlMRKQ-CyR-orw65j63sp1`
+      )
+      .pipe(res);
+  }
+});
 // Start the server
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`);
